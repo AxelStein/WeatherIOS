f8a4184a6db5823dcdfe4e784780416c7ad5fc1b
alex <diefaust1993@gmail.com>
2022-08-21 14:19:35 +0300

    Redesign day forecast item

--- Weather/Cells/DayForecastTableViewCell.swift
+++ Weather/Cells/DayForecastTableViewCell.swift
@@ -8,15 +8,13 @@
 import UIKit
 
 class DayForecastTableViewCell: UITableViewCell {
-    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
     @IBOutlet weak var weatherIconView: UIImageView!
     @IBOutlet weak var weekDayLabel: UILabel!
-    @IBOutlet weak var minTempLabel: UILabel!
     
     func setForecast(_ data: ForecastData) {
         weekDayLabel.text = data.validDate?.weekdayText
-        maxTempLabel.text = data.maxTemp?.temperatureText
-        minTempLabel.text = data.minTemp?.temperatureText
+        tempLabel.text = "\(data.minTemp?.temperatureText ?? "") / \(data.maxTemp?.temperatureText ?? "")"
         weatherIconView.load(src: data.weather.iconURL)
     }
 }
